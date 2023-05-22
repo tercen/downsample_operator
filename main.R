9a94ffe413277e863cc1329ad6cdc93a4f0c4972
Thiago Monteiro <tsmonteiro@gmail.com>
2023-05-22 19:21:35 +0000

    flag fix

--- main.R
+++ main.R
@@ -2,18 +2,29 @@ library(tercen)
 library(plyr)
 library(dplyr)
 
+
+# http://127.0.0.1:5400/test/w/149395de86cec5641a6345ce78023e3b/ds/b01ca698-422d-482b-94ff-5cd33f922349
+options("tercen.workflowId" = "149395de86cec5641a6345ce78023e3b")
+options("tercen.stepId"     = "b01ca698-422d-482b-94ff-5cd33f922349")
 ctx = tercenCtx()
 
-if(!is.null(ctx$op.value('seed')) && !ctx$op.value('seed') == "NULL") set.seed(as.integer(ctx$op.value('seed')))
 
-min_n <- 0
-if(!is.null(ctx$op.value('min_n'))) min_n <- as.numeric(ctx$op.value('min_n')) 
+seed <- ctx$op.value('seed', as.integer, -1)
+if( seed > 0){
+  set.seed(seed)
+}
+# if(!is.null(ctx$op.value('seed')) && !ctx$op.value('seed') == "NULL") set.seed(as.integer(ctx$op.value('seed')))
+
+# min_n <- 0
+min_n <- ctx$op.value('min_n', as.double, 0)
+# if(!is.null(ctx$op.value('min_n'))) min_n <- as.numeric(ctx$op.value('min_n')) 
 
 downSample <- function (.ci, group) {
+  df <- data.frame(.ci, group = as.factor(group))  
   
-  df <- data.frame(.ci, group = as.factor(group))
   
   minClass <- max(min_n, min(table(group))) # if 0 (default) or lower than min samp size, get min samp size
+
   
   df$label <- 0
   
@@ -29,11 +40,12 @@ downSample <- function (.ci, group) {
 
 .ci <- c(ctx$select(".ci")[[1]])
 group <- c(as.factor(ctx$select(".colorLevels")[[1]]))
-dups <- !duplicated(.ci)
-group <- group[dups]
-.ci <- .ci[dups]
+# dups <- !duplicated(group)
+# group <- group[dups]
+# .ci <- .ci[dups]
 
 df <- downSample(.ci, group)
+print(df)
 df %>%
   ctx$addNamespace() %>%
   ctx$save()
